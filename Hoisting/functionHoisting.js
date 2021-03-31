b7a904e214d9b23bc9b6afa199f31428f36bfd9f
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-03-31 14:05:43 +0530

    ADDED: Hoisting

---
+++ Hoisting/functionHoisting.js
@@ -0,0 +1,18 @@
+sum(5,10);
+
+function sum (a,b){
+    add = a+b;
+    console.log(add);
+
+}
+//function declaration hositing will work 
+//inside paraentises  hoisting wont work
+//function expression hoisting wont work
+// fat arrow  hoisting wont work;
+//will not work
+
+// sum(10,10);
+// const  sum = (a,b) => {
+//     add = a + b;
+//     console.log(add);
+// }
---
+++ Hoisting/variableHoisting.js
@@ -0,0 +1,20 @@
+console.log(a);
+var a; 
+a = 10 ;
+
+
+
+//hoisting
+
+// var a = undefined; 
+// console.log(a);
+// a = 10 ;
+
+
+
+// x = 5; // Assign 5 to x
+
+// elem = document.getElementById("demo"); // Find an element
+// elem.innerHTML = x;                     // Display x in the element
+
+// var x; // Declare x
